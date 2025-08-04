7088683641dde84117e3f6c6abb6533f784eb5e9
Kofusu <kofusu@protonmail.com>
2025-08-04 14:01:52 +0700

    feat: adding DeveloperDTO and Platform DTO

--- Kofu GameHub v2/Data/DTOs/DeveloperDTO.swift
+++ Kofu GameHub v2/Data/DTOs/DeveloperDTO.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct DeveloperDTO: Identifiable, Decodable {
+    let id: Int
+    let name: String
+    let gamesCount: Int
+    let imageBackground: URL?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case gamesCount = "games_count"
+        case imageBackground = "image_background"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        gamesCount = try container.decode(Int.self, forKey: .gamesCount)
+        
+        if let imageBackgroundString = try container.decodeIfPresent(String.self, forKey: .imageBackground) {
+            imageBackground = URL(string: imageBackgroundString)!
+        } else {
+            imageBackground = nil
+        }
+    }
+}
--- Kofu GameHub v2/Data/DTOs/PlatformDTO.swift
+++ Kofu GameHub v2/Data/DTOs/PlatformDTO.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+struct PlatformDTO: Identifiable, Codable {
+    let id: Int
+    let name: String
+    let gamesCount: Int
+    let imageBackground: URL?
+    let image: URL?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, image
+        
+        case gamesCount = "games_count"
+        case imageBackground = "image_background"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        gamesCount = try container.decode(Int.self, forKey: .gamesCount)
+        
+        if let imageBackgroundString = try container.decodeIfPresent(String.self, forKey: .imageBackground) {
+            imageBackground = URL(string: imageBackgroundString)!
+        } else {
+            imageBackground = nil
+        }
+        
+        if let imageString = try container.decodeIfPresent(String.self, forKey: .image) {
+            image = URL(string: imageString)!
+        } else {
+            image = nil
+        }
+    }
+}
+
