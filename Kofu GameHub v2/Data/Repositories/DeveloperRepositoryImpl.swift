60508c29316777445914f9731f1759c10a77f1a7
Kofusu <kofusu@protonmail.com>
2025-08-05 22:25:25 +0700

    feat: adding api client and repository impl

---
+++ Kofu GameHub v2/Data/Repositories/DeveloperRepositoryImpl.swift
@@ -0,0 +1,8 @@
+//
+//  DeveloperRepositoryImpl.swift
+//  Kofu GameHub v2
+//
+//  Created by Hendratara Pratama on 05/08/25.
+//
+
+import Foundation
---
+++ Kofu GameHub v2/Data/Repositories/PlatformRepositoryImpl.swift
@@ -0,0 +1,8 @@
+//
+//  PlatformRepositoryImpl.swift
+//  Kofu GameHub v2
+//
+//  Created by Hendratara Pratama on 05/08/25.
+//
+
+import Foundation
